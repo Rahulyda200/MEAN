d2b838a170dd7cd14dd756cbd875fb0f9b809157
Rahulyda200 <rahulydv7275@gmail.com>
2024-10-24 09:47:48 +0530

    add whatsaap design

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -8,6 +8,7 @@ interface Message {
   sender: string;
   receiver: string;
   content: string;
+  timestamp: Date; 
 }
 
 @Component({
@@ -17,7 +18,7 @@ interface Message {
 })
 export class ChatComponent implements OnInit, OnDestroy {
   @Input() user: any; 
-  @Output() chatClosed = new EventEmitter<void>();
+  @Output() chatClosed = new EventEmitter();
 
   messageContent: string = '';
   messages: Message[] = []; 
@@ -36,82 +37,88 @@ export class ChatComponent implements OnInit, OnDestroy {
     this.sender = this.userService.getLoggedInUserName(); 
     this.receiverName = this.data.user.name; 
 
-    this.fetchMessages();
+    this.fetchMessages(); 
+    this.joinRoom(); 
 
-    // Join the chat room
-    this.joinRoom();
-
-    // Subscribe to incoming messages
     this.messageSubscription = this.userService.receiveMessages().subscribe((message: any) => {
       console.log('Received message from server:', message);
-  
-      // Push the received message to the chat window
       this.messages.push({
         sender: message.sender,
         receiver: message.receiver,
-        content: message.content
+        content: message.content,
+        timestamp: new Date() 
       });
     });
+
     this.userService.onDisconnect().subscribe(() => {
       console.log('You have been disconnected');
     });
   }
 
   ngOnDestroy() {
-    this.messageSubscription.unsubscribe();
+    this.messageSubscription.unsubscribe(); 
   }
 
   fetchMessages() {
     this.http.get<{ messages: Message[] }>('http://localhost:3000/api/messages').subscribe(response => {
-      this.messages = response.messages
+      this.messages = response.messages.filter(msg => 
+        (msg.sender === this.sender && msg.receiver === this.receiverName) || 
+        (msg.sender === this.receiverName && msg.receiver === this.sender)
+      ).map(msg => ({
+        sender: msg.sender,
+        content: msg.content, 
+        receiver: msg.receiver || this.receiverName,
+        timestamp: msg.timestamp || new Date() // Use existing timestamp or set to now
+      }));
       
-        .filter(msg => 
-            (msg.sender === this.sender && msg.receiver === this.receiverName) || 
-            (msg.sender === this.receiverName && msg.receiver === this.sender)
-        )
-        .map(msg => ({
-            sender: msg.sender,
-            content: msg.content, 
-            receiver: msg.receiver || this.receiverName,
-        }));
-        
-        console.log("fetch all messages",this.messages)
+      console.log("Fetched messages:", this.messages);
     });
-
   }
 
   joinRoom() {
     this.roomId = this.data.user._id;  
     this.userService.joinChatRoom(this.roomId);  
   }
+
   sendMessage() {
     if (!this.messageContent.trim()) return;
-  
+
     const messageData = {
       sender: this.sender,
       receiver: this.receiverName,
       content: this.messageContent,
-      roomId: this.roomId
+      roomId: this.roomId,
+      timestamp: new Date() 
     };
-  
+
     console.log('Sending message:', messageData);
-  
-    // Send the message to the server via the userService
+
     this.userService.sendMessage(messageData).subscribe({
       next: () => {
         console.log('Message sent successfully');
+        this.messages.push({
+          sender: this.sender,
+          receiver: this.receiverName,
+          content: this.messageContent,
+          timestamp: messageData.timestamp 
+        });
       },
       error: (err: any) => {
         console.error('Error sending message:', err);
       }
     });
-  
-    // Clear the message input field
-    this.messageContent = '';
+
+    this.messageContent = ''; 
   }
-  
-  
-  
+
+  isSameDate(date1: Date, date2: Date): boolean {
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    return d1.getDate() === d2.getDate() &&
+           d1.getMonth() === d2.getMonth() &&
+           d1.getFullYear() === d2.getFullYear();
+  }
+
   closeChat(): void {
     this.chatClosed.emit(); 
   }
